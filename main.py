a4116274568a83338fc99c3997c7a120d2d2de6c
Vidit Patel <viditpatel777@gmail.com>
2025-05-26 23:20:49 -0400

    feat: <solved visualization- confusion matrix>

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from xgboost import XGBClassifier
-
+import xgboost as xgb 
+import matplotlib.pyplot as plt  
+from sklearn.metrics import confusion_matrix
+import seaborn as sns
 # Load penguins dataset from seaborn
 import seaborn as sns
 penguins = sns.load_dataset("penguins")
@@ -30,5 +33,23 @@ from xgboost import XGBClassifier
 model = XGBClassifier()  # No need for use_label_encoder=False anymore
 
 model.fit(X_train,y_train)
+
+
+r=  model.score(X_test,y_test)
+
+print("model sentiment",r)
+
+y_pred = model.predict(X_test)
+cm = confusion_matrix(y_test,y_pred)
+
+
+plt.figure(figsize=(8,4))
+sns.heatmap(cm,annot=True)
+
+plt.show()
+
+
+
+
 
 
